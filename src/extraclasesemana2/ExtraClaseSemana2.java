4cc2bb2f8d76af4a890628ae7eae363a2cbefd11
rcampos0302 <rcampos0302@gmail.com>
2022-09-25 10:02:06 -0600

    Extraclase parte 1 lista

--- src/extraclasesemana2/ExtraClaseSemana2.java
+++ src/extraclasesemana2/ExtraClaseSemana2.java
@@ -4,6 +4,9 @@
  */
 package extraclasesemana2;
 
+import javax.swing.JOptionPane;
+
+
 /**
  *
  * @author ricar
@@ -15,6 +18,41 @@ public class ExtraClaseSemana2 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        byte antiguedad;
+        byte HoraSemanales;
+        float salarioHora;
+        double salarioBruto;
+        double salarioNeto;
+        
+        antiguedad  = Byte.parseByte(JOptionPane.showInputDialog("Digite su "
+                + "antiguedad en años en la empresa"));
+        HoraSemanales  = Byte.parseByte(JOptionPane.showInputDialog("Digite "
+                + "la cantidad de horas laboradas por semana"));
+        salarioHora  = Byte.parseByte(JOptionPane.showInputDialog("Digite su"
+                + " salario por hora"));
+        
+        salarioBruto = (HoraSemanales*salarioHora);
+        
+        if(antiguedad>=10){
+            salarioBruto = salarioBruto + (salarioBruto*0.20);
+            if(salarioBruto>=1000){
+                salarioNeto = salarioBruto - (salarioBruto*0.10);
+                JOptionPane.showMessageDialog(null,"Su salario neto"
+                        + " es: " + salarioNeto);
+            }
+            else if (salarioBruto>=2000){
+                salarioNeto = salarioBruto - (salarioBruto*0.15);
+                JOptionPane.showMessageDialog(null,"Su salario neto"
+                        + " es: " + salarioNeto);
+            }
+        }
+        
+        else {
+            
+             JOptionPane.showMessageDialog(null,"su salario es: "
+                     + salarioBruto);
+        }
+                  
     }
     
 }
